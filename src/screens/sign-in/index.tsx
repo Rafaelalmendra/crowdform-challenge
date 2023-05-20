7255b74c4ad8117df784228baa0dc74bb8a512f2
Rafael Almendra <rafaelalmendra28@gmail.com>
2023-05-20 20:35:40 -0300

    feat: adds logic to log into the account on the sign in screen

--- src/screens/sign-in/index.tsx
+++ src/screens/sign-in/index.tsx
@@ -1,15 +1,39 @@
 import { StatusBar } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { useForm } from "react-hook-form";
+import { Toast } from "react-native-toast-message/lib/src/Toast";
+
+// hooks
+import { useSignIn } from "hooks";
 
 // components
-import { Button, Header, Input, TextComponent } from "components";
+import { Button, Header, ControlledInput, TextComponent } from "components";
 
 // styles
 import * as S from "./styles";
 import { GlobalWrapper } from "styles";
 
+type SignInType = {
+  email: string;
+  password: string;
+};
+
 const SignInScreen = () => {
   const { navigate } = useNavigation();
+  const { handleSignIn, loadingSignIn } = useSignIn();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<SignInType>();
+
+  const onSubmit = async (data: SignInType) => {
+    await handleSignIn({
+      email: data.email,
+      password: data.password,
+    });
+  };
 
   return (
     <GlobalWrapper>
@@ -20,6 +44,7 @@ const SignInScreen = () => {
       />
 
       <Header />
+      <Toast position="top" />
 
       <S.Container>
         <TextComponent fontSize={18} fontFamily="semiBold" marginBottom={34}>
@@ -27,15 +52,47 @@ const SignInScreen = () => {
         </TextComponent>
 
         <S.FormContainer>
-          <Input placeholder="Enter your email" label="E-mail" />
-          <Input
-            placeholder="Enter your password"
-            isPasswordInput
+          <ControlledInput
+            name="email"
+            control={control}
+            label="E-mail"
+            placeholder="Enter your e-mail"
+            errorMessage={errors.email?.message}
+            rules={{
+              required: {
+                message: "Required field",
+                value: true,
+              },
+              pattern: {
+                message: "Enter a valid email",
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              },
+            }}
+          />
+
+          <ControlledInput
+            name="password"
+            control={control}
             label="Password"
+            placeholder="Enter your password"
+            errorMessage={errors.password?.message}
+            isPasswordInput
+            rules={{
+              required: {
+                message: "Required field",
+                value: true,
+              },
+              minLength: {
+                message: "Minimum 8 characters",
+                value: 8,
+              },
+            }}
           />
         </S.FormContainer>
 
-        <Button>Login</Button>
+        <Button onPress={handleSubmit(onSubmit)} isLoading={loadingSignIn}>
+          Login
+        </Button>
 
         <S.SignUpContainer>
           <TextComponent fontSize={12} fontFamily="regular" color="gray">
